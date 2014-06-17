494fbe3706621b893203d090090083b173df2900
Robert Eriksson <robert@pepzi.org>
2014-06-17 21:22:29 +0200

    Adds preInit, init and postInit placeholders.

--- src/main/java/com/pahimar/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/pahimar/letsmodreboot/LetsModReboot.java
@@ -1,4 +1,24 @@
 package com.pahimar.letsmodreboot;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(name = "Let's Mod Reboot", modid = "letsmodreboot", version = "1.7.2-1.0")
 public class LetsModReboot {
+    @Mod.EventHandler()
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
 }
